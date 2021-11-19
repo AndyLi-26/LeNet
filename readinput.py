1147e6a58eaa969fe15f11c28548e5d3896054ec
unknown <1953812182@qq.com>
2021-11-19 18:37:29 +0800

    read and data

---
+++ readinput.py
@@ -0,0 +1,14 @@
+def getdata(filename):
+    fptr=open(filename,'r')
+    fptr.readline()
+    x,y=[],[]
+    for l in fptr:
+        data=l.split(',')
+        data=list(map(int,data))
+        y.append(data[0])
+        x.append(data[1:])
+    fptr.close()
+    return x,y
+if __name__=="__main__":
+    x,y=getdata("mnist_test.csv")
+    
