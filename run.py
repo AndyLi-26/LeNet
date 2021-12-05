c81f2cd7863963ceaa042689986e5efae7727b0a
Elaine <1800220elaine@gmail.com>
2021-12-05 17:55:49 +1100

    Update run.py
    update shuffle, convert to numpy

--- run.py
+++ run.py
@@ -2,7 +2,20 @@ from readinput import *
 from LNN import *
 from random import shuffle,random
 from math import ceil
-if __name__=="__main__":
+import numpy 
+if __name__=="__main__":    
+
+    def shuffle_in_unison(a, b):
+        assert len(a) == len(b)
+        shuffled_a = numpy.empty(a.shape, dtype=a.dtype)
+        shuffled_b = numpy.empty(b.shape, dtype=b.dtype)
+        permutation = numpy.random.permutation(len(a))
+        for old_index, new_index in enumerate(permutation):
+            shuffled_a[new_index] = a[old_index]
+            shuffled_b[new_index] = b[old_index]
+        return shuffled_a, shuffled_b
+
+
     x,y=getdata("mnist_train.csv") 
     print("reading done")
     model=MLP(lr=0.05,s=0.01)
@@ -10,7 +23,9 @@ if __name__=="__main__":
     batch_size=256
     model.train=True
     for _ in range(10):
-        shuffle(x) ## TODO: shuffle x and y together
+        # shuffle(x) ## TODO: shuffle x and y together
+
+        x,y = shuffle_in_unison(x,y)
         for i in range(ceil(len(x)/batch_size)):
             pre_acc=model.update(x[i*batch_size:min((i+1)*batch_size,len(x))],y[i*batch_size:min((i+1)*batch_size,len(x))])
             print(i,pre_acc)
@@ -22,4 +37,3 @@ if __name__=="__main__":
     test_acc=model.test(x,y)
     print(test_acc)
         
-    
