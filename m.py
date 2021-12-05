ae91468f9d2d92dc7f355e1ad80e50c87233c73a
Elaine <1800220elaine@gmail.com>
2021-12-05 17:54:45 +1100

    Update m.py

--- m.py
+++ m.py
@@ -1,26 +1,31 @@
 ## use numpy do operation
-from math import exp
+# from math import exp
+import numpy as np
 def new(r,c):
+    return np.zeros((r,c))
     return [[0 for _ in range(c)] for _ in range(r)]
 
 def dot(a,b):
     assert len(a)==len(b), str(len(a))+'dot'+str(len(b))
     return sum([a[i]*b[i] for i in range(len(a))])
 
-def shape(A):
-    return len(A),len(A[0])
+# def shape(A):
+#     return len(A),len(A[0])
     
 def mul(A,B):
-    assert len(A[0])==len(B),str(shape(A))+" X "+str(shape(B))
-    C=new(len(A),len(B[0]))
-    for i in range(len(C)):
-        for j in range(len(C[0])):
-            C[i][j]=sum([A[i][k]*B[k][j] for k in range(len(A[0]))])
-    return C
+    return np.matmul(A,B)
+    
+    # assert A.shape[1]==B.shape[0],str(A.shape)+" X "+str(B.shape)    
+    # C=new(len(A),len(B[0]))
+    # for i in range(len(C)):
+    #     for j in range(len(C[0])):
+    #         C[i][j]=sum([A[i][k]*B[k][j] for k in range(len(A[0]))])
+    # return C
     
 def ele(A,B,op):
-    assert len(A)==len(B),str(shape(A))+" X "+str(shape(B))
-    assert len(A[0])==len(B[0]),str(shape(A))+" X "+str(shape(B))
+    #TODO find sth in numpy to replace this
+    assert len(A)==len(B),str(A.shape)+" X "+str(B.shape)
+    assert len(A[0])==len(B[0]),str(A.shape)+" X "+str(B.shape)
     assert op in ['+','-','*','/'], "op = "+op
     C=[]
     for i in range(len(A)):
@@ -34,22 +39,23 @@ def ele(A,B,op):
             C.append([A[i][j]/B[i][j] for j in range(len(A[i]))])
     return C
     
-def T(A):
-    C=new(A[0],A)
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            C[j][i]=A[i][j]
-    return C
+# def T(A):
+#     C=new(A[0],A)
+#     for i in range(len(A)):
+#         for j in range(len(A[0])):
+#             C[j][i]=A[i][j]
+#     return C
     
-def sig(L):
-    assert str(L[0]).isnumeric(), "Layer shape: "+str(shape(L))
-    return list(map(lambda x: 1/(1+math.exp(-x)),L))
+# def sig(L):
+#     assert str(L[0]).isnumeric(), "Layer shape: "+str(shape(L))
+#     return list(map(lambda x: 1/(1+math.exp(-x)),L))
 
 def ReLU(L,a=0):
-    return [[(i if i>0 else a*i) for i in j] for j in L]
+    # TODO: numpy
+    return np.maximum(a,L)
 
-def copy(A):
-  A=[]
+# def copy(A):
+#   A=[] 
   
 if __name__=="__main__":
     A=new(256,784)
